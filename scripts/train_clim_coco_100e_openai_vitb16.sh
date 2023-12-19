1ef1cd04321c6cea43475e56c9b0f39d90222ca4
ZwwWayne <wayne.zw@outlook.com>
2023-12-19 14:46:16 +0800

    debug

--- scripts/train_clim_coco_100e_openai_vitb16.sh
+++ scripts/train_clim_coco_100e_openai_vitb16.sh
@@ -4,6 +4,6 @@ torchrun --nproc_per_node 8 -m training.main --batch-size=128 --lr=1e-5 --wd=0.1
 --val-data data/coco/annotations/panoptic_val2017.json \
 --embed-path metadata/coco_panoptic_clip_hand_craft_ViTB16.npy --train-image-root data/coco/train2017 \
 --val-image-root data/coco/val2017  --cache-dir checkpoints --log-every-n-steps 50 \
---lock-image --save-frequency 100 --lock-image-unlocked-groups 12 --extract-type="v2" \
---name clim_coco_100_save100_test10_openai_vitb16_12layers --downsample-factor 16 --det-image-size 1024 \
+--lock-image --save-frequency 100 --lock-image-unlocked-groups 6 --extract-type="v2" \
+--name clim_coco_100_save100_test10_openai_vitb16_6layers --downsample-factor 16 --det-image-size 1024 \
 --alpha 0.7 --train-image-size 1024
