755fe4fc37c03f9903b65cc7bf1c335a3ff4880f
wusize <wusize@sensetime.com>
2023-12-20 12:38:33 +0800

    ceph

--- scripts/train_clim_coco_100e_openai_vitb16.sh
+++ scripts/train_clim_coco_100e_openai_vitb16.sh
@@ -2,7 +2,8 @@ torchrun --nproc_per_node 8 -m training.main --batch-size=32 --lr=1e-5 --wd=0.1 
 --model ViT-B-16 --pretrained openai --warmup 1000  --zeroshot-frequency 10 --dataset-type coco_caption  \
 --test-type coco_panoptic --train-data data/coco/wusize/captions_train2017_tags_allcaps.json \
 --val-data data/coco/annotations/panoptic_val2017.json \
---embed-path metadata/coco_panoptic_clip_hand_craft_ViTB16.npy --train-image-root data/coco/train2017 \
+--embed-path metadata/coco_panoptic_clip_hand_craft_ViTB16.npy --train-image-root="" \
+--train-ceph-root s3://openmmlab/datasets/detection/coco/train2017 \
 --val-image-root data/coco/val2017  --cache-dir checkpoints --log-every-n-steps 50 \
 --lock-image --save-frequency 100 --lock-image-unlocked-groups 6 --extract-type="v2" \
 --name clim_coco_100_save100_test10_openai_vitb16_6layers --downsample-factor 16 --det-image-size 1024 \
