9d89005f3cf0ca3fd6eb1411edd71a8d750eb091
wusize <wusize@sensetime.com>
2024-02-01 12:29:16 +0800

    lvis

--- ovdet/ovdet/datasets/__init__.py
+++ ovdet/ovdet/datasets/__init__.py
@@ -1,3 +1,4 @@
 from .coco_caption import CocoCaptionOVDDataset
 from .samplers.multi_source_sampler import CustomGroupMultiSourceSampler
 from .pipelines import CachedMosaicWithCaption, MultiChoicesMosaic
+from .cc3m_lvis_v1 import CC3MLVISV1Dataset
